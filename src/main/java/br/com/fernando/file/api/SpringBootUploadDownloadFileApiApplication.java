2949505f554060b14b0604d1a02f489c3f7c07ec
tigelah <fernando-guide@hotmail.com>
2019-12-27 10:16:49 -0300

    first commit

---
+++ src/main/java/br/com/fernando/file/api/SpringBootUploadDownloadFileApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernando.file.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadDownloadFileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadDownloadFileApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fernando/file/api/SpringBootUploadDownloadFileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernando.file.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadDownloadFileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
